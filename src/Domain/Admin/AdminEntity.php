51207108506afa8615142097a662fb6c4d165ebe
Shahzad Khan <shahzad.khan@smoobu.com>
2024-11-06 19:12:01 +0100

    updated

--- src/Domain/Admin/AdminEntity.php
+++ src/Domain/Admin/AdminEntity.php
@@ -4,23 +4,7 @@ declare(strict_types=1);
 
 namespace Domain\Admin;
 
-use Application\Niayi;
-use Application\test;
-use Doctrine\Common\Collections\ArrayCollection;
 class AdminEntity {
-    public ArrayCollection $analytics;
-
-
-    public function test(){
-        $test = new test();
-    }
-
-    public function newTest(){
-        $test = new Niayi();
-    }
-
-    public function thrid(){
-        $test = new Niayi();
-    }
+    public array $analytics;
 
 }
--- src/Domain/testEntity.php
+++ src/Domain/testEntity.php
@@ -4,17 +4,9 @@ declare(strict_types=1);
 
 namespace Domain;
 
-use Application\Niayi;
-use Application\test;
 class testEntity {
-    public ArrayCollection $analytics;
+    public array $analytics;
 
-    public function test(){
-        $test = new test();
-    }
 
-    public function needToBeTested(){
-        $test = new Niayi();
-    }
 
 }
