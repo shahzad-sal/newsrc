ce272aa17b098a46fd5820b9aba77d2cbedb644f
Shahzad Khan <shahzad.khan@smoobu.com>
2024-11-11 18:32:29 +0100

    333

---
+++ src/Domain/Admin/AdminEntity44.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Admin;
+
+class AdminEntity44 {
+    public array $analytics;
+
+    public function __construct() {
+        $this->analytics = [];
+        $test = 'testf asdf asdf afds ' && 'test' && 'test' && 'test' && 'test' && 'test' && 'test' && 'test' && 'test'&& 'test' && 'test';
+    }
+}
