39694a9bab1056ec5e13ad6fdd2b23fb2faace51
Shahzad Khan <shahzad.khan@smoobu.com>
2024-11-12 11:24:40 +0100

    new Pr

---
+++ src/Domain/Admin/AdminEntity55.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Admin;
+
+use Application\Niayi;
+
+class AdminEntity55 {
+    public array $analytics;
+
+    public function __construct() {
+        $this->analytics = [];
+        $test = 'testf asdf asdf afds ' && 'test' && 'test' && 'test' && 'test' && 'test' && 'test' && 'test' && 'test'&& 'test' && 'test';
+    }
+
+    public function aliniayi() {
+        $this->analytics = [
+            'name' => 'test',
+            'value' => 1,
+        ];
+
+        $new = new Niayi()
+    }
+}
